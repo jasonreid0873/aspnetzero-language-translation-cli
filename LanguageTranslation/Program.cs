ecb773aeca05e4bb9aa8ba8f2243b2ed0a251315
Jason Reid <jason.reid@assurestor.com>
2020-09-23 07:53:33 +0100

    Initial commit

---
+++ LanguageTranslation/Program.cs
@@ -0,0 +1,124 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.IO;
+using System.Xml;
+using CommandLine;
+
+namespace LanguageTranslation
+{
+    class Program
+    {
+        private static XmlDocument doc = new XmlDocument();
+
+        public class Options
+        {
+            [Option('l', "targetLanguage", Required = true, HelpText = "Target language for translation (fr = french, etc.")]
+            public string TargetLanguage { get; set; }
+
+            [Option('t', "targetFilename", Required = false, HelpText = "Target filename (Default = <sourceFilename>-<targetLanguage> | example portal-fr.xml")]
+            public string TargetFilename { get; set; }
+
+            [Option('s', "sourceLanguage", Required = false, HelpText = "Source filename (Default = portal.xml")]
+            public string SourceLanguage { get; set; } = "en";
+
+            [Option('t', "sourceFilename", Required = false, HelpText = "Source language (Defaults to en = english)")]
+            public string SourceFilename { get; set; } = "portal.xml";
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                string targetLanguage = o.TargetLanguage;
+                string sourceFile = o.SourceFilename;
+                string sourceLanguage = o.SourceLanguage;
+                string targetFile = o.TargetFilename;
+                if (targetFile == null) { targetFile = $"{sourceFile.Split(".")[0]}-{targetLanguage}.xml"; }
+
+                Console.WriteLine($"Starting translation of {sourceFile} to language code {targetLanguage}...");
+                XmlElement root = LoadXml(sourceFile);
+                if (root != null)
+                {
+                    TranslateXml(root, sourceLanguage, targetLanguage, targetFile);
+                }
+                Console.WriteLine($"Translation completed and saved as {targetFile}");
+            });
+        }
+
+        static XmlElement LoadXml(string sourceFile)
+        {
+            try
+            {
+                string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "xml");
+                string fileName = Path.Combine(path, sourceFile);
+                doc.Load(fileName);
+                return doc.DocumentElement;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION!!!");
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+        
+        static void TranslateXml(XmlElement root, string sourceLanguage, string targetLanguage, string targetFile)
+        {
+            try
+            {
+                XmlNode texts = root.SelectSingleNode("texts");
+                foreach (XmlElement text in texts.ChildNodes)
+                {
+                    string translation = GoogleTranslate(text.InnerText, sourceLanguage, targetLanguage);
+                    if (translation != null)
+                    {
+                        Console.WriteLine($"{text.InnerText} = {translation}");
+                        text.InnerText = translation;
+                    }
+                }
+
+                SaveXml(targetFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION!!!");
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void SaveXml(string targetFile)
+        {
+            try
+            {
+                string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "xml");
+                string fileName = Path.Combine(path, targetFile);
+                doc.Save(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION!!!");
+                Console.WriteLine(ex);
+            }
+        }
+
+        static string GoogleTranslate(string text, string sourceLanguage, string targetLanguage)
+        {
+            // Ensure you have created a service account and enabled the Google Translation API
+            // https://cloud.google.com/translate/docs/setup
+            // Ensure you have set the environment variable GOOGLE_APPLICATION_CREDENTIALS to point to your download service account key JSON file
+            // https://cloud.google.com/docs/authentication/getting-started
+            try
+            {
+                TranslationClient client = TranslationClient.Create();
+                TranslationResult result = client.TranslateText(text, targetLanguage, sourceLanguage);
+                return result.TranslatedText;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION!!!");
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# aspnetzero-language-translation-cli
+# ASP.NET Zero Language Translation Utility
 CLI utility to produce language translations of ASP.NET Zero .xml language files using Google Translate API
+
+Dependency on the Google Tranlsation v2 API, please review 
+https://cloud.google.com/translate/docs/setup
+https://cloud.google.com/docs/authentication/getting-started
+
+Once you have your service account key (in .json format), update your environment to include the GOOGLE_APPLICATION_CREDENTIALS pointer as described in the help links above.
+
+All xml files (source and translated files) will be stored in the ./xml/ directory. A sample portal.xml file is included in the ./xml/ directory with a cut-down list of entries for testing
